94eb12339cf3f73b8a2749a0ae1fdff0e851ad1b
Guilherme Garcia Ienke <gui.garciaa04@gmail.com>
2026-03-02 20:20:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+//const n1 = 2
+//const n2 = '3'
+//const n3 = n1 + n2 // correção implícita
+//console.log(n3)
+// console.log(4)
+
+
+// vetores ou listas 
+//v1 = []
+//console.log(v1.length)
+//v1[0]= 2
+//console.log(v1.length)
+//v1[10] = 25
+//console.log(v1.length)
+//console.log(v1)
+//for (let i = 0; i < v1.length; i++) {
+    //console.log(v1[i])
+//}   
+
+
+//funções e arrow function 
+
+//function hello(){
+//    console.log("oi",nome)
+//}
+//hello("nome")
+
+
+// função
+//const dobro = function(n){
+//    return n * 2
+//}
+//console.log(dobro(5))
+
+// função triplo
+//const triplo = function(n=5){
+//    return n * 3
+//}
+//console.log(triplo())
+//console.log(triplo(6))
+
+//função e arrow function
+const hello = () => {console.log("oi")}
+hello() 
